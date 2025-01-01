b7bf61b41a2d7002a697ca29c772c8abecfe114c
Thuner2007 <thuner2007@outlook.com>
2025-01-01 20:43:33 +0100

    feat: update env

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,5 +6,8 @@ set -e
 echo "${CLOUDFLARE_ORIGIN_CERTIFICATE}" > /etc/ssl/certificate.pem
 echo "${CLOUDFLARE_ORIGIN_CA_KEY}" > /etc/ssl/certificate.key
 
+echo "${CLOUDFLARE_ORIGIN_CERTIFICATE_REVSTICKS}" > /etc/ssl/revsticks/certificate.pem
+echo "${CLOUDFLARE_ORIGIN_CA_KEY_REVSTICKS}" > /etc/ssl/revsticks/certificate.key
+
 # Start Nginx in the foreground
 nginx -g 'daemon off;'
